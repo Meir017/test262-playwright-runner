e524128fbb1a51d93628b7759f8d65820d0e4b94
Meir Blachman <meirblachman@gmail.com>
2022-08-26 18:14:29 +0300

    use html report

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -2,6 +2,9 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
     fullyParallel: true,
+    reporter: [
+        ['html', { outputDir: 'test-results' }],
+    ],
     timeout: 1000 * 60 * 60,
 };
 
