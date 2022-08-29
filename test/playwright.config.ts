5dacb309368f2c58dfb3b0d64564aeba0b7e96c9
Meir Blachman <meirblachman@gmail.com>
2022-08-29 23:14:16 +0300

    add playwright actions

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -3,7 +3,7 @@ import { type PlaywrightTestConfig, devices } from "@playwright/test";
 const config: PlaywrightTestConfig = {
     fullyParallel: true,
     reporter: [
-        ['html', { outputDir: 'playwright-report' }],
+        ['html'],
     ],
     timeout: 1000 * 60 * 60,
     projects: [
