66ac0dc7b36f4c01882a9b9fb92c9021f96ad435
Meir Blachman <meirblachman@gmail.com>
2022-09-05 09:06:21 +0300

    tests: support onlyStrict flag

--- test/fixture.ts
+++ test/fixture.ts
@@ -29,7 +29,7 @@ export const ignoreTests = new Set([
     'test/built-ins/Function/prototype/toString/built-in-function-object.js'
 ]);
 const includeTests = [
-    'test/built-ins/Function/prototype/toString/'
+    'test/built-ins/Function/prototype/apply'
 ];
 export const testList = Object.keys(test262)
     .filter(test => includeTests.some(includeTest => test.startsWith(includeTest)));
