ff97354254e4247519cd2846c8a4cba3145dde06
Meir Blachman <meirblachman@gmail.com>
2022-09-09 10:44:01 +0300

    chore: extract test parsing logic to file

---
+++ test/parser.ts
@@ -0,0 +1,38 @@
+import * as test262 from '../resources/test262.json';
+import * as YAML from 'yaml';
+
+interface TestSpec {
+    es6id?: string;
+    esid?: string;
+    description: string;
+    info: string;
+    includes: string[];
+    features: string[];
+    flags: string[];
+}
+
+export function parseTestDefinition(testCase) {
+    const testDifinition = test262[testCase] as string;
+
+    const specStart = '/*---';
+    const specEnd = '---*/';
+    const startOfCode = testDifinition.indexOf(specEnd) + specEnd.length;
+    const testSpec = testDifinition.substring(testDifinition.indexOf(specStart) + specStart.length, testDifinition.indexOf(specEnd));
+    const spec: TestSpec = YAML.parse(testSpec);
+
+    const dependencies = ['assert.js', 'sta.js', ...(spec.includes || [])]
+
+    let code = testDifinition.substring(startOfCode);
+
+    if (spec.flags?.includes('onlyStrict')) {
+        code = `'use strict';` + code;
+    }
+
+    return {
+        raw: testDifinition,
+        harness: dependencies.map(dependency => `<script id="harness/${dependency}">${test262['harness/' + dependency]}</script>`),
+        rawCode: testDifinition.substring(startOfCode),
+        code,
+        spec
+    }
+}
