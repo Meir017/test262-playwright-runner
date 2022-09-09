0f92b2d956acff6c75bed96e372ac23694dcedbb
Meir Blachman <meirblachman@gmail.com>
2022-09-09 13:17:24 +0300

    devtops: use github reporter for actions (#22)

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -3,7 +3,7 @@ import { type PlaywrightTestConfig, devices } from "@playwright/test";
 const config: PlaywrightTestConfig = {
     fullyParallel: true,
     reporter: [
-        ['html'],
+        [process.env.CI ? 'github' : 'html']
     ],
     timeout: 1000 * 60 * 60,
     projects: [
